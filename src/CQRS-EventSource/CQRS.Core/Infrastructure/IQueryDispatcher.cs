5a24f0dffe38fd32be9ead1f64e3a6e6ece87420
BRACITS\tamim.arefin <tamim.arefin@bracits.com>
2025-04-11 20:03:30 +0600

    completeting functionality of core modules

---
+++ src/CQRS-EventSource/CQRS.Core/Infrastructure/IQueryDispatcher.cs
@@ -0,0 +1,8 @@
+﻿namespace CQRS.Core.Infrastructure
+{
+    public interface IQueryDispatcher<TEntity>
+    {
+        void RegisterHandler<TQuery>(Func<TQuery, Task<List<TEntity>>> handler) where TQuery : BaseQuery;
+        Task<List<TEntity>> SendAsync(BaseQuery query);
+    }
+}
